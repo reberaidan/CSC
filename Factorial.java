56e8e67b322216a7340b881278e148875bc35337
reberaidan <aidanweinreber@hotmail.com>
2021-09-13 12:16:43 -0500

    More

---
+++ Factorial.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Factorial {
+    
+    
+    public static void main(String[] args){
+
+        Scanner myObj = new Scanner(System.in);
+        int x = myObj.nextInt();
+        //double result = whileLoop(x);
+        //double result = forLoop(x);
+        int result = recursionLoop(x);
+        System.out.println(result);
+
+    }
+
+    static double whileLoop(int x){
+        double fact= 1;
+        while(x>0){
+            if(x==0){
+                fact = 1;
+        }
+            else{
+                fact = fact*(x);
+                System.out.println(fact);
+                x--;
+            }
+        }
+        return fact;
+    }
+    static double forLoop(int x){
+        double fact = 1;
+        for(int i = x;i>0;i--){
+            fact = fact*x;
+            x--;
+        }
+        return fact;
+    }
+
+    static int recursionLoop(int x){
+        if(x==1){
+            return 1;
+        }
+        else{
+            return x*recursionLoop(x-1);
+        }
+    }
+ }
+    
+
---
+++ Hanoi.java
@@ -0,0 +1,19 @@
+public class Hanoi {
+    
+
+    public static void main(String[] args){
+        tower(3,"A","C","B");
+    }
+
+    static void tower(int disks, String src, String dest, String spr){
+        if(disks==1){
+            System.out.println(src + "->" + dest);
+        }
+        else{
+            tower(disks - 1, src,spr,dest);
+
+            tower(1,src,dest,spr);
+            tower(disks-1,spr,dest,src);
+        }
+    }
+}
