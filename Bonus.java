9636e8b71a25b3775818ebec13064dfda9586338
reberaidan <80844544+reberaidan@users.noreply.github.com>
2021-09-13 00:49:29 -0500

    New Programs

---
+++ Bonus.java
@@ -0,0 +1,10 @@
+public class Bonus
+{
+public static void main(String[] args)
+    {
+        int i, n = 20;
+        for (i=0; i<n; n--)
+            System.out.print("-");
+        System.out.println();
+    }
+}
