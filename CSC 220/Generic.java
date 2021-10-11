e688fcbaa838775369262cea879a4ee7bf5ea386
reberaidan <aidanweinreber@hotmail.com>
2021-10-11 16:04:34 -0500

    d

--- CSC 220/Generic.java
+++ CSC 220/Generic.java
@@ -1,10 +1,16 @@
-public class Generic {
+class Generic {
     public static void main(String[] args){
-        int[] x = {1,2,3,4,5};
+        Integer[] x = {1,2,3,4,5};
         printArr(x);
+
+        Double[] y = {1.1, 2.2, 3.3, 4.4, 5.5};
+        printArr(y);
+
+        String[] z = {"this", "shit", "important", "stop", "sleeping"};
+        printArr(z);
     }    
 
-    public static void printArr(int[] arr){
+    public static <mT> void printArr(mT[] arr){
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
